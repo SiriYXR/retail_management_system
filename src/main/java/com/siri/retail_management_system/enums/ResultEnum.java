90ca614293b7d772f6f9b89ede7ea71f36743374
SiriYang <www.yangxinruei@qq.com>
2019-04-17 22:14:33 +0800

    添加关于商品实体的异常信息

--- src/main/java/com/siri/retail_management_system/enums/ResultEnum.java
+++ src/main/java/com/siri/retail_management_system/enums/ResultEnum.java
@@ -7,19 +7,25 @@ package com.siri.retail_management_system.enums;
  * @description: 结果枚举类型
  * @date 2019/4/15 14:02
  */
-public enum  ResultEnum {
-    UNKONW_ERROR(-1,"未知错误"),
-    SUCCESS(0,"成功"),
-    USERNAME_WRONG(101,"用户名不存在"),
-    PASSWORD_WRONG(102,"密码错误"),
-    ADMIN_ID_WRONG(103,"管理员ID不存在"),
-    ADMIN_ADD_ERROR(104,"管理员添加/保存失败"),
-    ADMIN_FIND_ERROR(105,"管理员查询失败"),
-    MEMBER_NAME_WRONG(201,"会员名不存在"),
-    MEMBER_ID_WRONG(202,"会员ID不存在"),
-    MEMBER_ADD_ERROR(203,"会员添加/保存失败"),
-    MEMBER_FIND_ERROR(204,"会员查询失败"),
+public enum ResultEnum {
+    UNKONW_ERROR(-1, "未知错误"),
+    SUCCESS(0, "成功"),
 
+    USERNAME_WRONG(101, "用户名不存在"),
+    PASSWORD_WRONG(102, "密码错误"),
+    ADMIN_ID_WRONG(103, "管理员ID不存在"),
+    ADMIN_ADD_ERROR(104, "管理员添加/保存失败"),
+    ADMIN_FIND_ERROR(105, "管理员查询失败"),
+
+    MEMBER_NAME_WRONG(201, "会员名不存在"),
+    MEMBER_ID_WRONG(202, "会员ID不存在"),
+    MEMBER_ADD_ERROR(203, "会员添加/保存失败"),
+    MEMBER_FIND_ERROR(204, "会员查询失败"),
+
+    MERCHANDISE_NAME_WRONG(201, "商品名不存在"),
+    MERCHANDISE_ID_WRONG(202, "商品ID不存在"),
+    MERCHANDISE_ADD_ERROR(203, "商品添加/保存失败"),
+    MERCHANDISE_FIND_ERROR(204, "商品查询失败"),
     ;
 
     private Integer code;
